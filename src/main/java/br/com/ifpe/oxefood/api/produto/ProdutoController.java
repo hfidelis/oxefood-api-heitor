eed97beb5f5492ec75e0d30fb971354e3f6a16f6
hfidelis <heitorc88@gmail.com>
2025-05-11 18:22:56 -0300

    feat(product): product GET routes

--- src/main/java/br/com/ifpe/oxefood/api/produto/ProdutoController.java
+++ src/main/java/br/com/ifpe/oxefood/api/produto/ProdutoController.java
@@ -4,13 +4,11 @@ import br.com.ifpe.oxefood.modelo.produto.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import br.com.ifpe.oxefood.modelo.produto.Produto;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/produto")
@@ -27,4 +25,13 @@ public class ProdutoController {
        return new ResponseEntity<Produto>(produto, HttpStatus.CREATED);
    }
 
+   @GetMapping
+   public List<Produto> listarTodos() {
+       return produtoService.listarTodos();
+   }
+
+   @GetMapping("/{id}")
+   public Produto obterPorID(@PathVariable Long id) {
+       return produtoService.obterPorID(id);
+   }
 }
