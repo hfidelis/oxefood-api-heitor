3abf66a3375ef468eb7e8ede37259cb53d5cd990
hfidelis <heitorc88@gmail.com>
2025-05-11 18:46:19 -0300

    feat(client): client PUT route

--- src/main/java/br/com/ifpe/oxefood/api/cliente/ClienteController.java
+++ src/main/java/br/com/ifpe/oxefood/api/cliente/ClienteController.java
@@ -34,4 +34,10 @@ public class ClienteController {
    public Cliente obterPorID(@PathVariable Long id) {
       return clienteService.obterPorID(id);
    }
+
+   @PutMapping("/{id}")
+   public ResponseEntity<Cliente> update(@PathVariable("id") Long id, @RequestBody ClienteRequest request) {
+      clienteService.update(id, request.build());
+      return ResponseEntity.ok().build();
+   }
 }
