f1fdebbe14d0db0ac9ef2dd5a7db2a7dbe059f17
hfidelis <heitorc88@gmail.com>
2025-05-11 18:47:08 -0300

    feat(delivery-man): deliveryman PUT method

--- src/main/java/br/com/ifpe/oxefood/modelo/entregador/EntregadorService.java
+++ src/main/java/br/com/ifpe/oxefood/modelo/entregador/EntregadorService.java
@@ -1,5 +1,6 @@
 package br.com.ifpe.oxefood.modelo.entregador;
 
+import br.com.ifpe.oxefood.modelo.cliente.Cliente;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -26,5 +27,29 @@ public class EntregadorService {
 
    public Entregador obterPorID(Long id) {
        return repository.findById(id).get();
+   }
+
+   @Transactional
+   public void update(Long id, Entregador entregadorAlterado) {
+
+       Entregador entregador = repository.findById(id).get();
+
+       entregador.setNome(entregadorAlterado.getNome());
+       entregador.setDataNascimento(entregadorAlterado.getDataNascimento());
+       entregador.setCpf(entregadorAlterado.getCpf());
+       entregador.setRg(entregadorAlterado.getRg());
+       entregador.setFoneCelular(entregadorAlterado.getFoneCelular());
+       entregador.setFoneFixo(entregadorAlterado.getFoneFixo());
+       entregador.setQtdEntregasRealizadas(entregadorAlterado.getQtdEntregasRealizadas());
+       entregador.setValorFrete(entregadorAlterado.getValorFrete());
+       entregador.setEnderecoRua(entregadorAlterado.getEnderecoRua());
+       entregador.setEnderecoNumero(entregadorAlterado.getEnderecoNumero());
+       entregador.setEnderecoBairro(entregadorAlterado.getEnderecoBairro());
+       entregador.setEnderecoCidade(entregadorAlterado.getEnderecoCidade());
+       entregador.setEnderecoCep(entregadorAlterado.getEnderecoCep());
+       entregador.setEnderecoComplemento(entregadorAlterado.getEnderecoComplemento());
+       entregador.setEnderecoUf(entregadorAlterado.getEnderecoUf());
+
+       repository.save(entregador);
    }
 }
