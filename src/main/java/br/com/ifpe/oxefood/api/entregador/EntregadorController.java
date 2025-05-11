ab2c866ad1d6e7344ce77589a2f4314e416550bb
hfidelis <heitorc88@gmail.com>
2025-05-11 18:23:42 -0300

    feat(delivery-man): deliveryman GET routes

--- src/main/java/br/com/ifpe/oxefood/api/entregador/EntregadorController.java
+++ src/main/java/br/com/ifpe/oxefood/api/entregador/EntregadorController.java
@@ -3,14 +3,12 @@ package br.com.ifpe.oxefood.api.entregador;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import br.com.ifpe.oxefood.modelo.entregador.Entregador;
 import br.com.ifpe.oxefood.modelo.entregador.EntregadorService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/entregador")
@@ -27,4 +25,13 @@ public class EntregadorController {
        return new ResponseEntity<Entregador>(entregador, HttpStatus.CREATED);
    }
 
+   @GetMapping
+   public List<Entregador> listarTodos() {
+       return entregadorService.listarTodos();
+   }
+
+   @GetMapping("/{id}")
+   public Entregador obterPorID(@PathVariable Long id) {
+       return entregadorService.obterPorID(id);
+   }
 }
