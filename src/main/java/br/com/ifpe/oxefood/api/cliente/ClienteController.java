1944bd125fcb996a342b2fce85c259d23038a299
hfidelis <heitorc88@gmail.com>
2025-05-11 18:22:25 -0300

    feat(client): client GET routes

--- src/main/java/br/com/ifpe/oxefood/api/cliente/ClienteController.java
+++ src/main/java/br/com/ifpe/oxefood/api/cliente/ClienteController.java
@@ -3,14 +3,12 @@ package br.com.ifpe.oxefood.api.cliente;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import br.com.ifpe.oxefood.modelo.cliente.Cliente;
 import br.com.ifpe.oxefood.modelo.cliente.ClienteService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/cliente")
@@ -27,4 +25,13 @@ public class ClienteController {
        return new ResponseEntity<Cliente>(cliente, HttpStatus.CREATED);
    }
 
+   @GetMapping
+   public List<Cliente> listarTodos() {
+      return clienteService.listarTodos();
+   }
+
+   @GetMapping("/{id}")
+   public Cliente obterPorID(@PathVariable Long id) {
+      return clienteService.obterPorID(id);
+   }
 }
