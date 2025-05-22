1bddf5fb4d85a5745dfd14c737b381a5676652cf
hfidelis <heitorc88@gmail.com>
2025-05-22 20:20:59 -0300

    feat(product): delete route

--- src/main/java/br/com/ifpe/oxefood/api/produto/ProdutoController.java
+++ src/main/java/br/com/ifpe/oxefood/api/produto/ProdutoController.java
@@ -1,14 +1,22 @@
 package br.com.ifpe.oxefood.api.produto;
 
-import br.com.ifpe.oxefood.modelo.produto.ProdutoService;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import br.com.ifpe.oxefood.modelo.produto.Produto;
-
-import java.util.List;
+import br.com.ifpe.oxefood.modelo.produto.ProdutoService;
 
 @RestController
 @RequestMapping("/api/produto")
@@ -39,5 +47,11 @@ public class ProdutoController {
    public ResponseEntity<Produto> update(@PathVariable("id") Long id, @RequestBody ProdutoRequest request) {
        produtoService.update(id, request.build());
        return ResponseEntity.ok().build();
+   }
+
+   @DeleteMapping("/{id}")
+   public ResponseEntity<Void> delete(@PathVariable Long id) {
+      produtoService.delete(id);
+      return ResponseEntity.ok().build();
    }
 }
