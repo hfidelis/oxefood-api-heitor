9f647413eb0f5b5280ed4bf4f8143960d5016a23
hfidelis <heitorc88@gmail.com>
2025-05-22 20:21:31 -0300

    feat(delivery-man): delete route

--- src/main/java/br/com/ifpe/oxefood/api/entregador/EntregadorController.java
+++ src/main/java/br/com/ifpe/oxefood/api/entregador/EntregadorController.java
@@ -1,14 +1,22 @@
 package br.com.ifpe.oxefood.api.entregador;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import br.com.ifpe.oxefood.modelo.entregador.Entregador;
 import br.com.ifpe.oxefood.modelo.entregador.EntregadorService;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/entregador")
@@ -39,5 +47,11 @@ public class EntregadorController {
    public ResponseEntity<Entregador> update(@PathVariable("id") Long id, @RequestBody EntregadorRequest request) {
        entregadorService.update(id, request.build());
        return ResponseEntity.ok().build();
+   }
+
+   @DeleteMapping("/{id}")
+   public ResponseEntity<Void> delete(@PathVariable Long id) {
+      entregadorService.delete(id);
+      return ResponseEntity.ok().build();
    }
 }
