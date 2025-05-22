5856ff0b2cd5fe715d7272f49b248ebba261b8ef
hfidelis <heitorc88@gmail.com>
2025-05-22 20:20:33 -0300

    feat(client): delete route

--- src/main/java/br/com/ifpe/oxefood/api/cliente/ClienteController.java
+++ src/main/java/br/com/ifpe/oxefood/api/cliente/ClienteController.java
@@ -1,14 +1,22 @@
 package br.com.ifpe.oxefood.api.cliente;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import br.com.ifpe.oxefood.modelo.cliente.Cliente;
 import br.com.ifpe.oxefood.modelo.cliente.ClienteService;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/cliente")
@@ -40,4 +48,11 @@ public class ClienteController {
       clienteService.update(id, request.build());
       return ResponseEntity.ok().build();
    }
+
+   @DeleteMapping("/{id}")
+   public ResponseEntity<Void> delete(@PathVariable Long id) {
+      clienteService.delete(id);
+      return ResponseEntity.ok().build();
+   }
+
 }
